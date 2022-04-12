a4ac23e08eabc6a3eda1b064e9e7a05948fbeb5c
MuriungiMartin <muriungimmwiti@gmail.com>
2022-04-12 10:24:39 +0300

    COmmit 4

--- lib/ErrorStrings.dart
+++ lib/ErrorStrings.dart
@@ -1,40 +1,40 @@
-// ignore_for_file: file_names, non_constant_identifier_names, avoid_print
+// ignore_for_file: file_names, non_constant_identifier_names
 
 class ErrorStrings {
     static TestfieldError() {
     String testField = "The field Cannot be empty \n Please insert correct value!";
-    print(testField);
+    return(testField);
   }
   static emailValidateError() {
     String emailValidate = "Please Enter A valid e-mail address.";
-    print(emailValidate);
+    return(emailValidate);
   }
   static idvalidateerro(){
     String idValidate = "Id Number Cannot be less than Four Digits";
-    print(idValidate);
+    return(idValidate);
   }
   static chequevalidateerror(){
     String chequeValidate = "Cheque Number cannot have less than Six digits.";
-    print(chequeValidate);
+    return(chequeValidate);
   }
   static approvalerror(){
     String approvalError = "The Document's Status must be Approved Before Posting. \n Please Contact Your Approver.";
-    print(approvalError);
+    return(approvalError);
   }
   static invalidloanatype(){
       String LoantypeError = "The loan type does not exist \n Please enter a valid loan type from the listr provided.";
-      print(LoantypeError);
+      return(LoantypeError);
   }
   static invalidloanapplication(){
       String InvalidloanapplicationError="Approved Amount cannot be greater than amount applied.";
-      print(InvalidloanapplicationError);
+      return(InvalidloanapplicationError);
   }
   static immaturemember(){
       String Immaturemembererror="The member Must have saved with the sacco for more than six months to qualify for the loan";
-      print(Immaturemembererror);
+      return(Immaturemembererror);
   }
   static dateerror(){
       String Dateerror="Invalid Date";
-      print(Dateerror);
+      return(Dateerror);
   }
 }
